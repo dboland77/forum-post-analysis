8e4e60f8bf36618d2ace6efeb12fd0a90c2c3ee1
David Boland <dboland77@gmail.com>
2020-10-26 15:10:58 +0000

    Data grouped by month with top 3 topics

--- src/Posts.js
+++ src/Posts.js
@@ -3,38 +3,55 @@ import React, { Fragment } from "react";
 import { gql, useQuery } from "@apollo/client";
 import * as Util from "./Utilities";
 
+//Posts by creation month
 const GET_POSTS = gql`
-  query getPosts($count: Int!) {
-    allPosts(count: $count) {
-      id
-      title
-      body
-      published
-      createdAt
-      author {
-        id
-        firstName
-        lastName
-        email
-        avatar
-      }
-      likelyTopics {
-        label
-        likelihood
-      }
+query getPosts($count: Int!) {
+  allPosts(count: $count) {
+    id
+    title
+    published
+    createdAt
+    likelyTopics {
+      label
+      likelihood
     }
   }
+}
 `;
 
 const Posts = ({ onPostSelected }) => {
   const { loading, error, data } = useQuery(GET_POSTS, {
-    variables: { count: 8 },
+    variables: { count: 10},
   });
 
   if (loading) return "Loading...";
   if (error) return `Error! ${error.message}`;
-  console.log(Util.getDateFromEpoch(data.allPosts[0].createdAt));
- 
+
+  let addMonthName = data.allPosts.map(item => {
+    return{
+      ...item,
+      month: Util.monthNames[Util.getDateFromEpoch(item.createdAt).getMonth()]
+    };
+  })
+
+  let myTopics = addMonthName.map(item => {
+    return{
+      month: item.month,
+      likelyTopics: item.likelyTopics.slice(0,3)
+    };
+  })
+
+  console.log(Util.sortLikelihood(myTopics))
+
+  let group = myTopics.reduce((r,a)=>{
+    console.log("a",a);
+    console.log("r",r);
+    r[a.month] = [...r[a.month] || [], a];
+    return r;
+  },{});
+
+  console.log("group",group)
+
 
   return (
     <Fragment>
--- src/Utilities.js
+++ src/Utilities.js
@@ -6,3 +6,31 @@ export function getDateFromEpoch(dt) {
   d.setUTCMilliseconds(dt); // Add the number of milliseconds returned to get the date
   return d;
 }
+
+// export function aggregateByMonth(data) {
+//   return 1;
+// }
+
+// export function aggregateByAuthor(data) {
+//   return 1;
+// }
+
+
+export const monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
+
+//Sorts the likelihoods from most to least likely
+export function sortLikelihood(topics){
+   return  topics.sort(
+       function(a,b)
+       {
+        if(a.likelihood < b.likelihood)
+            return -1;
+        if(a.likelihood > b.likelihood)
+            return 1;
+        return 0
+    })
+}
+
+
